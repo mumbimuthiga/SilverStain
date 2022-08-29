44955829886f106eb62cb1712a04d7a507260a61
mumbimuthiga <veronicmuthiga@gmail.com>
2022-08-29 18:42:47 +0300

    Updated projects

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CompanyCRUDController;
+use App\Http\Controllers\EmployeeCRUDController;
 
 /*
 |--------------------------------------------------------------------------
